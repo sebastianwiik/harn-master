b82569f20e81a9ef2ae8013abe45b6c7dc420cc9
Sebastian Wiik <sebastian.wiik@hitta.se>
2020-03-05 16:33:53 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to harn-master
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/harn_master/core_test.clj
@@ -0,0 +1,7 @@
+(ns harn-master.core-test
+  (:require [clojure.test :refer :all]
+            [harn-master.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
