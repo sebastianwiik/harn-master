1b05709eeb6e0bfec03a4850ff63dddd53bd0bbd
Sebastian Wiik <sebastian.wiik@hitta.se>
2020-03-05 16:57:59 +0100

    added dices

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject harn-master "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Turn Simulator for Harn Master Role Playing Game"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot harn-master.core
   :target-path "target/%s"
--- src/harn_master/core.clj
+++ src/harn_master/core.clj
@@ -1,7 +1,18 @@
 (ns harn-master.core
   (:gen-class))
 
+(def d4 4)
+(def d6 6)
+(def d8 8)
+(def d10 10)
+(def d20 20)
+(def d100 100)
+
+(defn dice-roll
+  [dice]
+  (+ (rand-int dice) 1))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Simulates Harn Master Role Playing Game."
   [& args]
   (println "Hello, World!"))
